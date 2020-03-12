4998e028e79b387c285a2991c9d940001e3df791
Dylan Moore <dylan.moore@insanitycorr.com>
2020-03-12 16:05:25 -0400

    Paddles move networked

--- SDL-NetFramework/SDL-NetFramework/RemotePaddleBehaviour.cpp
+++ SDL-NetFramework/SDL-NetFramework/RemotePaddleBehaviour.cpp
@@ -3,6 +3,8 @@
 #include "Net.h"
 #include "glm/glm.hpp"
 
+#include "Sprite.h"
+
 RemotePaddleBehaviour::RemotePaddleBehaviour(bool server, sockaddr* addrinfo, int namelen)
 {
 	m_server = server;
@@ -23,6 +25,12 @@ void RemotePaddleBehaviour::init()
 void RemotePaddleBehaviour::update()
 {
 	std::string message = Net::recvFromUDP(m_addrinfo, &m_namelen);
+	if (message == "") return;
+		
 	glm::vec2 position;
 	sscanf_s(message.c_str(), "%f,%f", &position.x, &position.y);
+	position.x = 1280 - position.x;
+	position.y = 720 - position.y;
+
+	getSprite()->setPosition(position);
 }
